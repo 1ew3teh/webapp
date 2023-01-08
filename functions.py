e2776fd5795afa65c193131b0983552ddb8d5f7b
User_ananas <nastaskachenko02@gmail.com>
2023-01-08 02:12:33 +0100

    initial commot

--- functions.py
+++ functions.py
@@ -7,4 +7,4 @@ def get_todos(filepath=FILEPATH):
 
 def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
-        file.w ritelines(todos_arg)
+        file.writelines(todos_arg)
